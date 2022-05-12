3f4a88345f20475f715d94dc772e56f878a193c6
Bradley Owens <bradowens17@hotmail.com>
2022-05-12 16:41:39 +1000

    Upload project files

---
+++ script.js
@@ -0,0 +1,219 @@
+// inputs
+const citySearch = document.querySelector(".search");
+const searchBtn = document.getElementById("button");
+
+//time and info
+const dateTime = document.getElementById("date-time");
+const cityLocation = document.getElementById("location");
+
+//Current data
+const temp = document.getElementById("temperature");
+const weatherDescription = document.getElementById("weather-description");
+const windSpeed = document.getElementById("windSpeed");
+const humidity = document.getElementById("humidity");
+const icon = document.getElementById("icon");
+
+//Hourly forecast
+
+const hours = document.querySelectorAll(".hour");
+const td1 = document.querySelector(".first-td");
+const icon1 = document.querySelector(".first-icon");
+const temp1 = document.querySelector(".first-temp");
+
+const td2 = document.querySelector(".second-td");
+const icon2 = document.querySelector(".second-icon");
+const temp2 = document.querySelector(".second-temp");
+
+const td3 = document.querySelector(".third-td");
+const icon3 = document.querySelector(".third-icon");
+const temp3 = document.querySelector(".third-temp");
+
+const td4 = document.querySelector(".fourth-td");
+const icon4 = document.querySelector(".fourth-icon");
+const temp4 = document.querySelector(".fourth-temp");
+
+const td5 = document.querySelector(".fifth-td");
+const icon5 = document.querySelector(".fifth-icon");
+const temp5 = document.querySelector(".fifth-temp");
+
+searchBtn.addEventListener("click", function () {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      citySearch.value +
+      "&appid=18668a4c2ad515c5b7e803dfd6a0af25&units=metric"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data["message"] === "city not found") {
+        alert("Not a valid city!");
+      }
+
+      //Location (city & country)
+      const valueCityLocation = data["name"];
+      const valueCountry = data["sys"]["country"];
+      const geoLocation = `${valueCityLocation}, ${valueCountry}`;
+
+      // Description
+      const description = data["weather"][0].description;
+      const correctDescription = description.replace(
+        description[0],
+        description[0].toUpperCase()
+      );
+      const feelsLikeTemp = String(data["main"]["feels_like"]);
+      const stringDescription = `Feels like ${feelsLikeTemp.slice(
+        0,
+        -2
+      )}°C. ${correctDescription}`;
+
+      //Windspeed
+      const valueWindSpeed = `Wind: ${data["wind"]["speed"]}km`;
+
+      //Humidity
+      const valueHumidity = `Humidity: ${data["main"]["humidity"]}%`;
+
+      //Main temperature
+      const locationTemp = String(data["main"]["temp"]);
+      const valueTemp = `${locationTemp.slice(0, -2)}°C`;
+
+      //icon
+      const valueIcon = data["weather"][0].icon;
+
+      //date and time
+      const date = String(new Date(data["dt"] * 1000));
+
+      //Applying values to DOM
+
+      cityLocation.innerHTML = geoLocation;
+      weatherDescription.innerHTML = stringDescription;
+      windSpeed.innerHTML = valueWindSpeed;
+      humidity.innerHTML = valueHumidity;
+      temp.innerHTML = valueTemp;
+      icon.innerHTML = valueIcon;
+      icon.src = `http://openweathermap.org/img/wn/${valueIcon}.png`;
+      dateTime.innerHTML = date.slice(0, -52);
+
+      // weekly forecast------------------------------------------
+
+      fetch(
+        "http://api.openweathermap.org/data/2.5/forecast?q=" +
+          citySearch.value +
+          "&appid=dbab33003a27afd19ca1089574e71c9b&units=metric"
+      )
+        .then((response) => response.json())
+        .then((dataFive) => {
+          console.log(dataFive);
+
+          //looping info from API and storing in arrays
+          const dateArr = [];
+          const tempArr = [];
+          const iconArr = [];
+
+          for (var i = 0; i < dataFive.list.length; i += 8) {
+            const dateInfo = dataFive.list[i].dt_txt;
+            const tempInfo = dataFive.list[i].main.temp;
+            const iconInfo = dataFive.list[i].weather[0].icon;
+            iconArr.push(iconInfo);
+            tempArr.push(tempInfo);
+            dateArr.push(dateInfo);
+          }
+
+          //Days----------------------------------------------------
+
+          //looping date to get formatted day (mon-tues-wed)
+          // const dayArr = [];
+          // for (var i = 0; i < dateArr.length; i++) {
+          //   var options = { weekday: "long" };
+          //   const value = new Date(dateArr[i]);
+          //   const dayString = String(
+          //     new Intl.DateTimeFormat("en-US", options).format(value)
+          //   );
+          //   const slicedDay = dayString.slice(0, 3);
+          //   dayArr.push(slicedDay);
+          // }
+
+          const dayArr = dateArr.map(function (arr) {
+            var options = { weekday: "long" };
+            const value = new Date(arr);
+            const dayString = String(
+              new Intl.DateTimeFormat("en-US", options).format(value)
+            );
+            return dayString.slice(0, 3);
+          });
+
+          //Destructuring and assigning
+          const [firstD, secondD, thirdD, fourthD, fifthD] = dayArr;
+          td1.innerHTML = firstD;
+          td2.innerHTML = secondD;
+          td3.innerHTML = thirdD;
+          td4.innerHTML = fourthD;
+          td5.innerHTML = fifthD;
+
+          //
+          //
+          //Temp-----------------------------------------------------
+
+          //looping info from array and formatting into (21°C)
+          const formattedTempArr = [];
+          for (let i = 0; i < tempArr.length; i++) {
+            const valueTemp = String(tempArr[i]);
+            const formattedTemp = `${valueTemp.slice(0, -2)}°C`;
+            formattedTempArr.push(formattedTemp);
+          }
+
+          //Destructuring and assigning
+          const [firstT, secondT, thirdT, fourthT, fifthT] = formattedTempArr;
+          temp1.innerHTML = firstT;
+          temp2.innerHTML = secondT;
+          temp3.innerHTML = thirdT;
+          temp4.innerHTML = fourthT;
+          temp5.innerHTML = fifthT;
+
+          //
+          //
+          //
+          //
+          //
+          //
+
+          //icon--------------------------------------------------------------
+          //Destructuring and assigning
+          const [firstI, secondI, thirdI, fourthI, fifthI] = iconArr;
+          icon1.src = `http://openweathermap.org/img/wn/${firstI}.png`;
+          icon2.src = `http://openweathermap.org/img/wn/${secondI}.png`;
+          icon3.src = `http://openweathermap.org/img/wn/${thirdI}.png`;
+          icon4.src = `http://openweathermap.org/img/wn/${fourthI}.png`;
+          icon5.src = `http://openweathermap.org/img/wn/${fifthI}.png`;
+        });
+
+      //
+      //
+      //
+      //
+      //
+      //
+
+      // Unpslash API for background IMG
+
+      fetch(
+        `https://api.unsplash.com/search/photos/?client_id=dbMN8qb4WA4zxiF3YJ5w7CizLQ-ajiZ7p7xYUD0njzI&query=${citySearch.value}`
+      )
+        .then((response) => response.json())
+        .then((dataImg) => {
+          // console.log(dataImg);
+
+          const backgroundImg =
+            dataImg.results[Math.trunc(Math.random() * 9) + 1]["urls"].regular;
+          // console.log(backgroundImg);
+
+          const bodyBackground = document.querySelector(".body");
+
+          bodyBackground.style.backgroundImage = "url(" + backgroundImg + ")";
+
+          /////////////////////////////////////
+          //Clear input field
+          // citySearch.value = "";
+          // CHANGE BEFORE REPOSIT
+          //////////////////////////////////////
+        });
+    });
+});
