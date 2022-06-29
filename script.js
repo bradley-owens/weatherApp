2d5690abbca202481507f60f2767674b2c862313
Bradley Owens <bradowens17@hotmail.com>
2022-06-29 17:18:41 +1000

    cleaning fetch and ppromise code and adding error catch for loss of internet connection

--- script.js
+++ script.js
@@ -105,89 +105,94 @@ searchBtn.addEventListener("click", function () {
 
       // weekly forecast------------------------------------------
 
-      fetch(
+      return fetch(
         "http://api.openweathermap.org/data/2.5/forecast?q=" +
           citySearch.value +
           "&appid=dbab33003a27afd19ca1089574e71c9b&units=metric"
-      )
-        .then((response) => response.json())
-        .then((dataFive) => {
-          //looping info from API and storing in arrays
-          const dateArr = [];
-          const tempArr = [];
-          const iconArr = [];
+      );
+    })
+    .then((response) => response.json())
+    .then((dataFive) => {
+      //looping info from API and storing in arrays
+      const dateArr = [];
+      const tempArr = [];
+      const iconArr = [];
 
-          for (var i = 0; i < dataFive.list.length; i += 8) {
-            const dateInfo = dataFive.list[i].dt_txt;
-            const tempInfo = dataFive.list[i].main.temp;
-            const iconInfo = dataFive.list[i].weather[0].icon;
-            iconArr.push(iconInfo);
-            tempArr.push(tempInfo);
-            dateArr.push(dateInfo);
-          }
+      for (var i = 0; i < dataFive.list.length; i += 8) {
+        const dateInfo = dataFive.list[i].dt_txt;
+        const tempInfo = dataFive.list[i].main.temp;
+        const iconInfo = dataFive.list[i].weather[0].icon;
+        iconArr.push(iconInfo);
+        tempArr.push(tempInfo);
+        dateArr.push(dateInfo);
+      }
 
-          //Days----------------------------------------------------
+      //Days----------------------------------------------------
 
-          const dayArr = dateArr.map(function (arr) {
-            var options = { weekday: "long" };
-            const value = new Date(arr);
-            const dayString = String(
-              new Intl.DateTimeFormat("en-US", options).format(value)
-            );
-            return dayString.slice(0, 3);
-          });
+      const dayArr = dateArr.map(function (arr) {
+        var options = { weekday: "long" };
+        const value = new Date(arr);
+        const dayString = String(
+          new Intl.DateTimeFormat("en-US", options).format(value)
+        );
+        return dayString.slice(0, 3);
+      });
 
-          //Destructuring and assigning
-          const [firstD, secondD, thirdD, fourthD, fifthD] = dayArr;
-          td1.innerHTML = firstD;
-          td2.innerHTML = secondD;
-          td3.innerHTML = thirdD;
-          td4.innerHTML = fourthD;
-          td5.innerHTML = fifthD;
+      //Destructuring and assigning
+      const [firstD, secondD, thirdD, fourthD, fifthD] = dayArr;
+      td1.innerHTML = firstD;
+      td2.innerHTML = secondD;
+      td3.innerHTML = thirdD;
+      td4.innerHTML = fourthD;
+      td5.innerHTML = fifthD;
 
-          //Temp-----------------------------------------------------
+      //Temp-----------------------------------------------------
 
-          const formattedTempArr = tempArr.map(function (arr) {
-            const valueTemp = arr.toFixed(0);
-            return `${valueTemp}°C`;
-          });
+      const formattedTempArr = tempArr.map(function (arr) {
+        const valueTemp = arr.toFixed(0);
+        return `${valueTemp}°C`;
+      });
 
-          //Destructuring and assigning
-          const [firstT, secondT, thirdT, fourthT, fifthT] = formattedTempArr;
-          temp1.innerHTML = firstT;
-          temp2.innerHTML = secondT;
-          temp3.innerHTML = thirdT;
-          temp4.innerHTML = fourthT;
-          temp5.innerHTML = fifthT;
+      //Destructuring and assigning
+      const [firstT, secondT, thirdT, fourthT, fifthT] = formattedTempArr;
+      temp1.innerHTML = firstT;
+      temp2.innerHTML = secondT;
+      temp3.innerHTML = thirdT;
+      temp4.innerHTML = fourthT;
+      temp5.innerHTML = fifthT;
 
-          //icon--------------------------------------------------------------
-          //Destructuring and assigning
-          const [firstI, secondI, thirdI, fourthI, fifthI] = iconArr;
-          icon1.src = `http://openweathermap.org/img/wn/${firstI}.png`;
-          icon2.src = `http://openweathermap.org/img/wn/${secondI}.png`;
-          icon3.src = `http://openweathermap.org/img/wn/${thirdI}.png`;
-          icon4.src = `http://openweathermap.org/img/wn/${fourthI}.png`;
-          icon5.src = `http://openweathermap.org/img/wn/${fifthI}.png`;
-        });
+      //icon--------------------------------------------------------------
+      //Destructuring and assigning
+      const [firstI, secondI, thirdI, fourthI, fifthI] = iconArr;
+      icon1.src = `http://openweathermap.org/img/wn/${firstI}.png`;
+      icon2.src = `http://openweathermap.org/img/wn/${secondI}.png`;
+      icon3.src = `http://openweathermap.org/img/wn/${thirdI}.png`;
+      icon4.src = `http://openweathermap.org/img/wn/${fourthI}.png`;
+      icon5.src = `http://openweathermap.org/img/wn/${fifthI}.png`;
 
       // Unpslash API for background IMG -------------------------------------------------------
 
-      fetch(
+      return fetch(
         `https://api.unsplash.com/search/photos/?client_id=dbMN8qb4WA4zxiF3YJ5w7CizLQ-ajiZ7p7xYUD0njzI&query=${citySearch.value}`
-      )
-        .then((response) => response.json())
-        .then((dataImg) => {
-          // console.log(dataImg);
+      );
+    })
+    .then((response) => response.json())
+    .then((dataImg) => {
+      // console.log(dataImg);
 
-          const backgroundImg =
-            dataImg.results[Math.trunc(Math.random() * 9) + 1]["urls"].regular;
-          // console.log(backgroundImg);
+      const backgroundImg =
+        dataImg.results[Math.trunc(Math.random() * 9) + 1]["urls"].regular;
+      // console.log(backgroundImg);
 
-          const bodyBackground = document.querySelector(".body");
+      const bodyBackground = document.querySelector(".body");
 
-          bodyBackground.style.backgroundImage = "url(" + backgroundImg + ")";
+      bodyBackground.style.backgroundImage = "url(" + backgroundImg + ")";
 
-          citySearch.value = "";
-        });
+      citySearch.value = "";
+    })
+    .catch((err) => {
+      console.error(` Something went wrong Error:${err}`);
+
+      alert(` Something went wrong Error:${err}`);
     });
 });
