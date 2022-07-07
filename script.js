0983be0ad906637cc3257d39e053b7231891873e
Bradley Owens <bradowens17@hotmail.com>
2022-07-07 17:34:01 +1000

    clean code from promise chaining to async/await code

--- script.js
+++ script.js
@@ -36,159 +36,145 @@ const td5 = document.querySelector(".fifth-td");
 const icon5 = document.querySelector(".fifth-icon");
 const temp5 = document.querySelector(".fifth-temp");
 
-const getJSON = function (url, errMsg = "Uh Oh!") {
-  return fetch(url).then((response) => {
-    if (!response.ok) {
-      throw new Error(`${errMsg} Could not find country (${response.status}) `);
-    }
-    return response.json();
-  });
-};
-
-searchBtn.addEventListener("click", function () {
-  getJSON(
+searchBtn.addEventListener("click", async function () {
+  const responseMain = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${citySearch.value}&appid=18668a4c2ad515c5b7e803dfd6a0af25&units=metric`
-  )
-    .then((data) => {
-      //Location (city & country)
-      const valueCityLocation = data["name"];
-      const valueCountry = data["sys"]["country"];
-      const geoLocation = `${valueCityLocation}, ${valueCountry}`;
+  );
+  const dataMain = await responseMain.json();
 
-      // Description
-      const description = data["weather"][0].description;
-      const correctDescription = description.replace(
-        description[0],
-        description[0].toUpperCase()
-      );
-      const feelsLikeTemp = data["main"]["feels_like"].toFixed(0);
-      const stringDescription = `Feels like ${feelsLikeTemp}°C. ${correctDescription}`;
+  //Location (city & country)
+  const valueCityLocation = dataMain["name"];
+  const valueCountry = dataMain["sys"]["country"];
+  const geoLocation = `${valueCityLocation}, ${valueCountry}`;
 
-      //Windspeed
-      const valueWindSpeed = `Wind: ${data["wind"]["speed"]}km`;
+  // Description
+  const description = dataMain["weather"][0].description;
+  const correctDescription = description.replace(
+    description[0],
+    description[0].toUpperCase()
+  );
+  const feelsLikeTemp = dataMain["main"]["feels_like"].toFixed(0);
+  const stringDescription = `Feels like ${feelsLikeTemp}°C. ${correctDescription}`;
 
-      //Humidity
-      const valueHumidity = `Humidity: ${data["main"]["humidity"]}%`;
+  //Windspeed
+  const valueWindSpeed = `Wind: ${dataMain["wind"]["speed"]}km`;
 
-      //Main temperature
-      const locationTemp = data["main"]["temp"].toFixed(0);
-      const valueTemp = `${locationTemp}°C`;
+  //Humidity
+  const valueHumidity = `Humidity: ${dataMain["main"]["humidity"]}%`;
 
-      //icon
-      const valueIcon = data["weather"][0].icon;
+  //Main temperature
+  const locationTemp = dataMain["main"]["temp"].toFixed(0);
+  const valueTemp = `${locationTemp}°C`;
 
-      //date and time
+  //icon
+  const valueIcon = dataMain["weather"][0].icon;
 
-      let date = new Date(data["dt"] * 1000);
-      const options = {
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-        weekday: "long",
-      };
+  //date and time
 
-      const formattedDate = new Intl.DateTimeFormat("en-AU", options).format(
-        date
-      );
+  let date = new Date(dataMain["dt"] * 1000);
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    weekday: "long",
+  };
 
-      //Applying values to DOM
+  const formattedDate = new Intl.DateTimeFormat("en-AU", options).format(date);
 
-      cityLocation.innerHTML = geoLocation;
-      weatherDescription.innerHTML = stringDescription;
-      windSpeed.innerHTML = valueWindSpeed;
-      humidity.innerHTML = valueHumidity;
-      temp.innerHTML = valueTemp;
-      icon.innerHTML = valueIcon;
-      icon.src = `http://openweathermap.org/img/wn/${valueIcon}.png`;
-      dateTime.innerHTML = formattedDate;
+  //Applying values to DOM
 
-      // weekly forecast------------------------------------------
+  cityLocation.innerHTML = geoLocation;
+  weatherDescription.innerHTML = stringDescription;
+  windSpeed.innerHTML = valueWindSpeed;
+  humidity.innerHTML = valueHumidity;
+  temp.innerHTML = valueTemp;
+  icon.innerHTML = valueIcon;
+  icon.src = `http://openweathermap.org/img/wn/${valueIcon}.png`;
+  dateTime.innerHTML = formattedDate;
 
-      return getJSON(
-        `http://api.openweathermap.org/data/2.5/forecast?q=${citySearch.value}&appid=dbab33003a27afd19ca1089574e71c9b&units=metric`
-      );
-    })
-    .then((dataFive) => {
-      //looping info from API and storing in arrays
-      const dateArr = [];
-      const tempArr = [];
-      const iconArr = [];
+  // weekly forecast------------------------------------------
 
-      for (var i = 0; i < dataFive.list.length; i += 8) {
-        const dateInfo = dataFive.list[i].dt_txt;
-        const tempInfo = dataFive.list[i].main.temp;
-        const iconInfo = dataFive.list[i].weather[0].icon;
-        iconArr.push(iconInfo);
-        tempArr.push(tempInfo);
-        dateArr.push(dateInfo);
-      }
+  const responseWeekly = await fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?q=${citySearch.value}&appid=dbab33003a27afd19ca1089574e71c9b&units=metric`
+  );
 
-      //Days----------------------------------------------------
+  const dataWeekly = await responseWeekly.json();
 
-      const dayArr = dateArr.map(function (arr) {
-        var options = { weekday: "long" };
-        const value = new Date(arr);
-        const dayString = String(
-          new Intl.DateTimeFormat("en-US", options).format(value)
-        );
-        return dayString.slice(0, 3);
-      });
+  //looping info from API and storing in arrays
+  const dateArr = [];
+  const tempArr = [];
+  const iconArr = [];
 
-      //Destructuring and assigning
-      const [firstD, secondD, thirdD, fourthD, fifthD] = dayArr;
-      td1.innerHTML = firstD;
-      td2.innerHTML = secondD;
-      td3.innerHTML = thirdD;
-      td4.innerHTML = fourthD;
-      td5.innerHTML = fifthD;
+  for (var i = 0; i < dataWeekly.list.length; i += 8) {
+    const dateInfo = dataWeekly.list[i].dt_txt;
+    const tempInfo = dataWeekly.list[i].main.temp;
+    const iconInfo = dataWeekly.list[i].weather[0].icon;
+    iconArr.push(iconInfo);
+    tempArr.push(tempInfo);
+    dateArr.push(dateInfo);
+  }
 
-      //Temp-----------------------------------------------------
+  //Days----------------------------------------------------
 
-      const formattedTempArr = tempArr.map(function (arr) {
-        const valueTemp = arr.toFixed(0);
-        return `${valueTemp}°C`;
-      });
+  const dayArr = dateArr.map(function (arr) {
+    var options = { weekday: "long" };
+    const value = new Date(arr);
+    const dayString = String(
+      new Intl.DateTimeFormat("en-US", options).format(value)
+    );
+    return dayString.slice(0, 3);
+  });
 
-      //Destructuring and assigning
-      const [firstT, secondT, thirdT, fourthT, fifthT] = formattedTempArr;
-      temp1.innerHTML = firstT;
-      temp2.innerHTML = secondT;
-      temp3.innerHTML = thirdT;
-      temp4.innerHTML = fourthT;
-      temp5.innerHTML = fifthT;
+  //Destructuring and assigning
+  const [firstD, secondD, thirdD, fourthD, fifthD] = dayArr;
+  td1.innerHTML = firstD;
+  td2.innerHTML = secondD;
+  td3.innerHTML = thirdD;
+  td4.innerHTML = fourthD;
+  td5.innerHTML = fifthD;
 
-      //icon--------------------------------------------------------------
-      //Destructuring and assigning
-      const [firstI, secondI, thirdI, fourthI, fifthI] = iconArr;
-      icon1.src = `http://openweathermap.org/img/wn/${firstI}.png`;
-      icon2.src = `http://openweathermap.org/img/wn/${secondI}.png`;
-      icon3.src = `http://openweathermap.org/img/wn/${thirdI}.png`;
-      icon4.src = `http://openweathermap.org/img/wn/${fourthI}.png`;
-      icon5.src = `http://openweathermap.org/img/wn/${fifthI}.png`;
+  //Temp-----------------------------------------------------
 
-      // Unpslash API for background IMG -------------------------------------------------------
+  const formattedTempArr = tempArr.map(function (arr) {
+    const valueTemp = arr.toFixed(0);
+    return `${valueTemp}°C`;
+  });
 
-      return getJSON(
-        `https://api.unsplash.com/search/photos/?client_id=dbMN8qb4WA4zxiF3YJ5w7CizLQ-ajiZ7p7xYUD0njzI&query=${citySearch.value}`
-      );
-    })
-    .then((dataImg) => {
-      // console.log(dataImg);
+  //Destructuring and assigning
+  const [firstT, secondT, thirdT, fourthT, fifthT] = formattedTempArr;
+  temp1.innerHTML = firstT;
+  temp2.innerHTML = secondT;
+  temp3.innerHTML = thirdT;
+  temp4.innerHTML = fourthT;
+  temp5.innerHTML = fifthT;
 
-      const backgroundImg =
-        dataImg.results[Math.trunc(Math.random() * 9) + 1]["urls"].regular;
-      // console.log(backgroundImg);
+  //icon--------------------------------------------------------------
+  //Destructuring and assigning
+  const [firstI, secondI, thirdI, fourthI, fifthI] = iconArr;
+  icon1.src = `http://openweathermap.org/img/wn/${firstI}.png`;
+  icon2.src = `http://openweathermap.org/img/wn/${secondI}.png`;
+  icon3.src = `http://openweathermap.org/img/wn/${thirdI}.png`;
+  icon4.src = `http://openweathermap.org/img/wn/${fourthI}.png`;
+  icon5.src = `http://openweathermap.org/img/wn/${fifthI}.png`;
 
-      const bodyBackground = document.querySelector(".body");
+  // Unpslash API for background IMG -------------------------------------------------------
 
-      bodyBackground.style.backgroundImage = "url(" + backgroundImg + ")";
+  const responseImg = await fetch(
+    `https://api.unsplash.com/search/photos/?client_id=dbMN8qb4WA4zxiF3YJ5w7CizLQ-ajiZ7p7xYUD0njzI&query=${citySearch.value}`
+  );
 
-      citySearch.value = "";
-    })
-    .catch((err) => {
-      alert(err);
-    });
+  const dataImg = await responseImg.json();
+
+  const backgroundImg =
+    dataImg.results[Math.trunc(Math.random() * 9) + 1]["urls"].regular;
+  // console.log(backgroundImg);
+
+  const bodyBackground = document.querySelector(".body");
+
+  bodyBackground.style.backgroundImage = "url(" + backgroundImg + ")";
+
+  citySearch.value = "";
 });
